52da7dde414878cba3a332b7b7d649872771348c
samuel <difrentecho@gmail.com>
2017-09-26 16:58:45 +0100

    Adds more test cases

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,3 +1,5 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
 const Computer = require('../src/computer');
 const Desktop = require('../src/desktop');
 const Laptop = require('../src/laptop');
@@ -10,14 +12,69 @@ harddisk
 speed
 */
 describe('Computer', () => {
-    let Computer = new Computer('Dell', 'Grey', '16Gb', '1Tb', '4.5Hz');
+    let computer = new Computer('Dell', 'Grey', '16Gb', '1Tb', '4.5Hz');
     //handleling valid inputs
     describe('Creates an Object', () => {
         // Instatiation
-        it('should create object if instantiated with valid input and return 3 as live', function() {
-            assert.equal(Computer.manufacturer(), 'Dell');
+        it('Should return Dell as Manufacturers Name', function() {
+            assert.equal(computer.manufacturer, 'Dell');
         });
 
     });
 
+    describe('Handles Invalid input', () => {
+        // Instatiation
+        let result = function () {
+          return  computer = new Computer(1, 'Grey', '16Gb', '1Tb', '4.5Hz');
+        }
+        
+        it('Should return TypeError', function() {
+            expect(result).to.throw(TypeError, 'Invalid input type')
+        });
+
+      });     
+
+});
+  
+describe('Desktop', () => {
+    let desktop = new Desktop('Dell', 'Grey', '16Gb', '1Tb', '4.5Hz', 'LCD');
+    //handleling valid inputs
+    describe('Creates an Object and demonstrate Inheritance', () => {
+        // Instatiation
+        it('should retun Dell as manufactures name', function() {
+            assert.equal(desktop.manufacturer, 'Dell');
+          });
+          
+           // Instatiation
+        it('should return LCD as screen ', function() {
+            assert.equal(desktop.screenType, 'LCD');
+        });
+          
+        it('Should override fanNoise Method', function() {
+            assert.equal(desktop.fanNoise(), 'whossssh');
+        });
+
+    });
 })
+    
+describe('Laptop', () => {
+      let laptop = new Laptop('Dell', 'Grey', '16Gb', '1Tb', '4.5Hz', 'LiOn');
+      //handleling valid inputs
+      describe('Creates an Object and demonstrate Inheritance', () => {
+          // Instatiation
+          it('should retun Dell as manufactures name', function() {
+              assert.equal(laptop.manufacturer, 'Dell');
+            });
+            
+            // Instatiation
+          it('should return LiOn as battery name', function() {
+              assert.equal(laptop.batteryType, 'LiOn');
+          });
+            
+          it('Should override fanNoise Method', function() {
+              assert.equal(laptop.fanNoise(), 'sssssssshhh');
+          });
+
+      });
+  });
+  
